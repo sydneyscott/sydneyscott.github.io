770a47c6d887bd513003657625ca8f03f5f663d0
Sydney <scottsy@oregonstate.edu>
2019-10-09 15:38:46 -0700

    Orange Thing/ Small Name

---
+++ main.js
@@ -0,0 +1,7 @@
+<script>
+      document.addEventListener("DOMContentLoaded", function() {
+        document.getElementById("mainHeader").onclick = function() {
+          this.style.color = 'orange'
+        }
+      })
+      </script>
